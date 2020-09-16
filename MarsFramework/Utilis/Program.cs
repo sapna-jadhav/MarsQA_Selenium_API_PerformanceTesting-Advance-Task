6dc2d6c3cdf95685ba6bdbd14fdeffd80ba88a3d
sapna-jadhav <42228469+sapna-jadhav@users.noreply.github.com>
2020-09-16 16:38:32 +1000

    Update Program.cs

--- MarsFramework/Utilis/Program.cs
+++ MarsFramework/Utilis/Program.cs
@@ -23,12 +23,83 @@ namespace MarsFramework
 
 
 		[Test]
-		public static void Test()
+		public static void TestLogin()
 		{
-			//SignIn objsignin = new SignIn();
-			//objsignin.LoginSteps();
+			SignIn objsignin = new SignIn();
+			objsignin.LoginSteps();
+		}
+			
+		[Test]
+		public static void TestLanguage()
+		{
+			Skills objLanguage = new Skills();
+			objLanguage.AddLanguage();
+			objLanguage.EditLanguage();
+			objLanguage.DeleteLanguage();
+		}
+		[Test]
+		public static void TestSkills()
+		{
+			Skills objSkills = new Skills();
+			objSkills.AddSkills();
+			objSkills.EditSkills();
+			objSkills.DeleteSkills();
 			
 		}
+			[Test]
+		public static void TestEducation()
+		{
+			Skills objeducation = new Skills();
+			objeducation.AddEducation();
+			objeducation.EditEduaction();
+			objeducation.DeleteEducation();
+		}
+		[Test]
+		public static void TestCertificate()
+		{
+			Skills objcertificate = new Skills();
+			objcertificate.AddCertificate();
+			objcertificate.EditCertificate();
+			objcertificate.DeleteCertificate();
+
+		}
+		[Test]
+		public static void TestProfile()
+		{
+			Profile objprofile = new Profile();
+			objprofile.EditProfile();
+
+		}
+		[Test]
+		public static void TestSearch()
+		{
+			Search objsearch = new Search();
+			objsearch.SearchSkill();
+
+		}
+		[Test]
+		public static void TestRegister()
+		{
+			SignUp objregister = new SignUp();
+			objregister.register();
+
+		}
+		[Test]
+		public static void TestShareSkill()
+		{
+			ShareSkill objshareskill = new ShareSkill();
+			objshareskill.EnterShareSkill();
+
+		}
+		[Test]
+		public static void TestManageListings()
+		{
+			ManageListings objListings = new ManageListings();
+			objListings.ViewListing();
+			objListings.DeleteListing();
+
+		}
+	
 
 
 		[AfterFeature]
